1b7fb9f6e51c20a246ea435d4ba9f0042b7689e4
Steph-en <appahstephen9@gmail.com>
2021-11-17 14:56:20 +0000

    debuged code

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,7 +1,5 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e44ab4dc1faac81ca80800619bf1059a&page=1'
-
-cont IMG_PATH = 'https://image.tmdb.org/t/p/w128000'
-
+const IMG_PATH = 'https://image.tmdb.org/t/p/w128000'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=e44ab4dc1faac81ca80800619bf1059a&&query="'
 
 const main = document.getElementById('main')
@@ -62,8 +60,9 @@ form.addEventListener('submit', (e) => {
 
   const searchTerm = search.value
 
-  if (searchTerm && searchTerm == !'') {
+  if (searchTerm && searchTerm !== '') {
     getMovies(SEARCH_API + searchTerm)
+
     search.value = ''
   } else {
     window.location.reload()
